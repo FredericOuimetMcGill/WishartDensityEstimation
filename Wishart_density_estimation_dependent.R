6a1ea0419448108fc784a36dd1c45fd725957288
FredericOuimetMcGill <frederic.ouimet2@mcgill.ca>
2024-10-15 13:12:51 -0400

    Update Wishart_density_estimation_dependent.R

--- Wishart_density_estimation_dependent.R
+++ Wishart_density_estimation_dependent.R
@@ -1259,7 +1259,7 @@ write.csv(raw_results, raw_output_file, row.names = FALSE)
 print("Raw results saved to raw_ISE_dependent.csv")
 
 #########################
-## Process the Results ##
+## Process the results ##
 #########################
 
 # Create a data frame to store the summary results
